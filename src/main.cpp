c67eb730c8f827f09ddff215a964809d4efe3827
Art Gonzalez <arturo.gonzalez@austincc.edu>
2018-09-25 12:20:01 -0500

    encapsulate clock, update unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,60 @@
 #include "clock.h"
+#include<thread>
+#include<iostream>
+
+class Timer
+{
+	std::thread th;
+	bool running = false;
+public:
+	typedef std::chrono::milliseconds Interval;
+	typedef std::function<void(void)> Timeout;
+
+	void start(const Interval &interval,
+		const Timeout &timeout)
+	{
+		running = true;
+		th = std::thread([=]()
+		{
+			while (running == true) {
+				std::this_thread::sleep_for(interval);
+				timeout();
+			}
+		});
+	}
+
+	void stop()
+	{
+		running = false;
+		th.join();
+	}
+};
 
 int main() 
 {
 	Clock clock;
+	/*Timer tHello;
+	
+	tHello.start(std::chrono::milliseconds(1000), [&] {
 
-	clock.display_time();
+		//auto t = std::time(nullptr);
+		clock.display_time();
+		std::cout <<std::endl;
+		//cout << std::ctime(&t) << std::endl;
+
+	
+	});*/
+	
+	int i{ 0 };
+	while (i != 100) 
+	{
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		clock.display_time();
+		std::cout << std::endl;
+		i++;
+	}
+	
+//	tHello.stop();
 	
 	return 0;
 }
